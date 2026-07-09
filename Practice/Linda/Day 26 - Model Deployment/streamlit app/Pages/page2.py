d302caeb145fdaf07bc4e4d9d0019961f9af2819
LindaAmin <lyndaamin82@gmail.com>
2026-07-09 09:25:06 +0800

    Commit 09 Jul

---
+++ Practice/Linda/Day 26 - Model Deployment/streamlit app/Pages/page2.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("Page 2 - ")
+st.write("we are building a streamlit app with multiple pages")
--- Practice/Linda/Day 26 - Model Deployment/streamlit app/app.py
+++ Practice/Linda/Day 26 - Model Deployment/streamlit app/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
-st.title("MultiPage Streamlit app")
+st.title("ML Model Predictions App")
 st.write("Choose the right application from below to use it for free")
 
-st.set_page_config(page_title="Assistant AI", layout="centered")
-st.title("Assistant AI")
+st.header("Home")
 
 
-st.page_link("Pages/breast_cancer.py", label="Breast Cancer")
-st.page_link("Pages/page2.py", label="Page 2")
+# keep page center adn give the browser tabs
+st.set_page_config(page_title= "Assistant AI", layout = 'centered')
+
+
+
+st.page_link("Pages/Breast_Cancer.py", label= "Breast Cancer App")
+st.page_link("Pages/page2.py", label= "Page 2")
